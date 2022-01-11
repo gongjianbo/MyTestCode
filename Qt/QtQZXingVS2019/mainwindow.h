78bd38b552b513f9c71d60256b41e123ce8d23b5
gongjianbo <654344883@qq.com>
2022-01-12 00:41:15 +0800

    qt qzxing

---
+++ Qt/QtQZXingVS2019/mainwindow.h
@@ -0,0 +1,23 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QImage>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QImage img;
+};
+#endif // MAINWINDOW_H
